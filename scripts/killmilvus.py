9334458ade59cd15544cac822037a11748e6f105
Kyle <kyle.murray100@gmail.com>
2025-01-24 22:33:57 -0800

    Cleaning up the repo of stale files

--- scripts/killmilvus.py
+++
@@ -1,8 +0,0 @@
-from src.Utilities.Utilities import find_milvus_proc
-
-running_milvus_proc = find_milvus_proc(19530)
-if running_milvus_proc:
-    print(f"Killing Milvus process {running_milvus_proc.pid}")
-    running_milvus_proc.kill()
-else:
-    print("Milvus process not found")
