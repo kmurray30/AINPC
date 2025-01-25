ce148308f550167fda51ca72c9e9a70442eeb3db
Kyle <kyle.murray100@gmail.com>
2025-01-25 04:06:25 -0800

    Interaction working very well when not using json responses

--- src/core/Constants.py
+++ src/core/Constants.py
@@ -8,3 +8,11 @@ class Role(Enum):
 class AgentName(Enum):
     pat = "Pat"
     mock_user = "Mock User"
+
+class Llm(Enum):
+    gpt_3_5_turbo = "gpt-3.5-turbo"
+    gpt_3_5_turbo_instruct = "gpt-3.5-turbo-instruct"
+    gpt_4o_mini = "gpt-4o-mini"
+    gpt_4o = "gpt-4o"
+    o1 = "o1-preview"
+    llama3 = "llama3"
